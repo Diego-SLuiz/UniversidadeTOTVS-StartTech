f91b4dde8bf9d91ba4306d4d5729220ac50997ba
Diego <CTT@SPON-010113866.sp01.local>
2022-11-18 08:55:47 -0300

    Concluindo atividade conversão de temperatura

--- ExerciciosCondicional/src/ConverterCelsiusFahrenheit.java
+++ ExerciciosCondicional/src/ConverterCelsiusFahrenheit.java
@@ -1,25 +1,26 @@
-import org.w3c.dom.ls.LSOutput;
-
 import java.util.Scanner;
 
 public class ConverterCelsiusFahrenheit {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
 
-        System.out.println("Digite o tipo de temperatura (F - Fahrenheit | C - Celsius)");
+        System.out.print("Digite o tipo de medida (F - Fahrenheit | C - Celsius): ");
         String medida = entrada.nextLine();
 
-        System.out.printf("Digite uma temperatura em %s\n", medida);
+        System.out.printf("Digite uma temperatura em °%s: ", medida);
         Double temperatura = entrada.nextDouble();
 
-        if (medida.equals("F")) {
-            System.out.printf("A medida %s em °C é %.2f\n", medida, );
+        if (medida.equalsIgnoreCase("F")) {
+            System.out.printf("A temperatura %s°F é %.1f°C\n", temperatura, (temperatura - 32) * 5 / 9 );
             return;
         }
 
+        if (medida.equalsIgnoreCase("C")) {
+            System.out.printf("A temperatura %s°C é %.1f°F\n", temperatura, (temperatura * 9 / 5) + 32);
+            return;
+        }
 
+        System.out.println("Tipo de medida inválida!");
     }
 
 }
-
-
