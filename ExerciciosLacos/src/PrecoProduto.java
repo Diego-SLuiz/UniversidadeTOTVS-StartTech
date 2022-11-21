4d4aff73e23363d8da1089893748807944caf5df
Diego <CTT@SPON-010113866.sp01.local>
2022-11-21 10:45:30 -0300

    Concluindo atividade comparar preço de produtos

--- ExerciciosLacos/src/PrecoProduto.java
+++ ExerciciosLacos/src/PrecoProduto.java
@@ -4,9 +4,27 @@ public class PrecoProduto {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
 
-        System.out.print("Digite o nome do produto: ");
-        System.out.print("Digite o preço do produto: ");
-        String nomeProdutoUm = entrada.nextLine();
-        Double precoProdutoUm = entrada.nextDouble();
+        String[] produtoNomes = new String[3];
+        Double[] produtoPrecos = new Double[3];
+        String nomeProdutoMaisCaro = "";
+        Double precoProdutoMaisCaro = 0.0;
+
+        for (int i = 0; i < 3; i++) {
+            System.out.print("Digite o nome de um produto: ");
+            produtoNomes[i] = entrada.nextLine();
+
+            System.out.printf("Digite o preço do produto \"%s\": ", produtoNomes[i]);
+            produtoPrecos[i] = Double.parseDouble(entrada.nextLine().replace(",", "."));
+
+            System.out.printf("Foi informado o produto %s que custa R$ %.2f\n", produtoNomes[i], produtoPrecos[i]);
+
+            if (produtoPrecos[i] > precoProdutoMaisCaro) {
+                nomeProdutoMaisCaro = produtoNomes[i];
+                precoProdutoMaisCaro = produtoPrecos[i];
+            }
+        }
+
+        System.out.printf("O produto mais caro é \"%s\"", nomeProdutoMaisCaro);
     }
+
 }
