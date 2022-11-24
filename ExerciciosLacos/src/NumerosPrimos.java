31e858aaf0f784e3366fd30ba85ce69f9fba2684
Diego <luizdiego.silvabarbosa@gmail.com>
2022-11-23 21:27:08 -0300

    Concluindo atividade busca por números primos

--- ExerciciosLacos/src/NumerosPrimos.java
+++ ExerciciosLacos/src/NumerosPrimos.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-m=
+
 public class NumerosPrimos {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
@@ -12,8 +12,26 @@ public class NumerosPrimos {
             return;
         }
 
-        for (int i = 0; i < numeroLimite; i++) {
+        System.out.printf("O número informado foi %d\n", numeroLimite);
 
-        }
+        for (int i = 0; i < numeroLimite; i++) {
+            Boolean primo = true;
+
+            if (i == 0 || i == 1) {
+                continue;
+            }
+
+            for (int j = 2; j < i; j++) {
+                if (i % j == 0) {
+                    primo = false;
+                    break;
+                }
+            }
+
+            if (primo) {
+                System.out.printf("O número %d é primo!\n", i);
+            }
+         }
     }
+
 }
