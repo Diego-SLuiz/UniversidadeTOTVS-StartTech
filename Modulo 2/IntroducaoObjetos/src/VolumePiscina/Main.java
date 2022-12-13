a7bf7dc3f254806795b9e88361f5bdfbc2be0991
Diego <luizdiego.silvabarbosa@gmail.com>
2022-12-13 20:59:33 -0300

    Concluindo atividade calcular volume piscina

---
+++ Modulo 2/IntroducaoObjetos/src/VolumePiscina/Main.java
@@ -0,0 +1,16 @@
+package VolumePiscina;
+
+import Utilidades.Leitor;
+
+public class Main {
+    public static void main(String[] args) {
+        String mensagemPadrao = "Digite o(a) %s da piscina: ";
+        String avisoPadrao = "Valor inválido";
+        Integer largura = Leitor.lerInteiro(mensagemPadrao.formatted("largura"), avisoPadrao);
+        Integer comprimento = Leitor.lerInteiro(mensagemPadrao.formatted("comprimento"), avisoPadrao);
+        Integer profundidade = Leitor.lerInteiro(mensagemPadrao.formatted("profundidade"), avisoPadrao);
+        Piscina piscina = new Piscina(largura, comprimento, profundidade);
+
+        System.out.printf("O volume dessa piscina é: %s³\n", piscina.calcularVolume());
+    }
+}
---
+++ Modulo 2/IntroducaoObjetos/src/VolumePiscina/Piscina.java
@@ -0,0 +1,17 @@
+package VolumePiscina;
+
+public class Piscina {
+    public Integer largura;
+    public Integer comprimento;
+    public Integer profundidade;
+
+    public Piscina(Integer largura, Integer comprimento, Integer profundidade) {
+        this.largura = largura;
+        this.comprimento = comprimento;
+        this.profundidade = profundidade;
+    }
+
+    public Integer calcularVolume() {
+        return largura * comprimento * profundidade;
+    }
+}
