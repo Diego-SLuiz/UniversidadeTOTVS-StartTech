237b026d830f38631e9f2f74073d15128966ef05
Diego <CTT@SPON-010113866.sp01.local>
2023-02-06 16:45:47 -0300

    Incluindo atividade sobre produtos

---
+++ Modulo 3/SQL/funcionarios.sql
@@ -0,0 +1,114 @@
+CREATE DATABASE clientes_db;
+USE clientes_db;
+
+CREATE TABLE cliente(
+	id_cliente INT NOT NULL IDENTITY(1, 1),
+	nome VARCHAR(255) NOT NULL,
+	email VARCHAR(255) NOT NULL,
+	cpf CHAR(11) NOT NULL,
+	sexo CHAR(1) NOT NULL,
+	idade INT NOT NULL,
+	PRIMARY KEY(id_cliente),
+);
+
+INSERT INTO cliente(nome, email, idade, sexo, cpf) VALUES ('Robson', 'email@email.com', 40,'M', '12312312312');
+INSERT INTO cliente(nome, email, idade, sexo, cpf) VALUES ('Denise', 'email@email.com', 35,'F', '18989888812');
+INSERT INTO cliente(nome, email, idade, sexo, cpf) VALUES ('Madalena', 'email@email.com', 55,'F', '34534534545');
+INSERT INTO cliente(nome, email, idade, sexo, cpf) VALUES ('Jorge', 'email@email.com', 60, 'M', '89089089090');
+INSERT INTO cliente(nome, email, idade, sexo, cpf) VALUES ('Ana', 'email@email.com', 18, 'F','90909009090');
+INSERT INTO cliente(nome, email, idade, sexo, cpf) VALUES ('Bruna', 'email@email.com', 26, 'F','78568945239');
+
+SELECT nome, idade, sexo 
+FROM cliente 
+WHERE sexo = 'F'
+	AND idade > 30
+ORDER BY idade DESC;
+
+CREATE TABLE departamento(
+	id_departamento INT NOT NULL IDENTITY(1, 1),
+	nome VARCHAR(255) NOT NULL
+	PRIMARY KEY(id_departamento)
+);
+
+CREATE TABLE funcionario(
+	id_funcionario INT NOT NULL IDENTITY(1, 1),
+	id_departamento  INT NOT NULL,
+	nome VARCHAR(255) NOT NULL,
+	email VARCHAR(255) NOT NULL,
+	salario INT NOT NULL,
+	data_nascimento DATE NOT NULL,
+	sexo CHAR(1) NOT NULL
+	PRIMARY KEY(id_funcionario)
+	FOREIGN KEY(id_departamento) REFERENCES departamento(id_departamento)
+);
+
+INSERT INTO departamento(nome) VALUES('Vendas'), 
+	('Marketing'), 
+	('RH'), 
+	('TI'), 
+	('Administrativo'), 
+	('Desenvolvimento');
+
+INSERT INTO funcionario(nome, email, sexo, data_nascimento, salario, id_departamento) VALUES
+	('Luana', 'luana@gmail.com', 'F', '01-05-1996', 3000, 1),
+	('Marilia', 'marilia@hotmail.com', 'F', '03-05-1995', 3100, 2),
+	('Raquel', 'raquel@gmail.com', 'F', '15-07-1994', 3050, 3),
+	('Joana', 'joana@gmail.com', 'F', '03-07-1995', 3000, 4),
+	('Fabricio', 'fabricio@gmail.com', 'M', '01-05-1993', 4700, 5),
+	('Everaldo', 'everaldo@hotmail.com', 'M', '07-05-1993', 4600, 6),
+	('Ricardo', 'ricardo@gmail.com', 'M', '15-05-1998', 3500, 1),
+	('Cleber', 'cleber@hotmail.com', 'M', '01-05-1997', 3300, 3),
+	('Fernando', 'fernando@hotmail.com', 'M', '03-05-1989', 3000, 2),
+	('Felipe', 'felipe@hotmail.com', 'M', '03-05-1996', 3070, 4),
+	('Matheus', 'matheus@hotmail.com', 'M', '15-05-1989', 4060, 5),
+	('Rodrigo', 'rodrigo@hotmail.com', 'M', '01-05-1996', 3060, 6),
+	('Luma', 'luma@gmail.com', 'F', '01-05-1996', 3700, 1),
+	('Rafael', 'rafael@gmail.com', 'M', '15-03-1991', 3500, 4),
+	('Mariana', 'mariana@gmail.com', 'F', '01-07-1998', 3900, 4),
+	('Monique', 'monique@gmail.com', 'F', '15-07-1993', 4100, 2),
+	('Joaquim', 'joaquim@hotmail.com', 'M', '01-05-1994', 3300, 3),
+	('Sandro', 'sandro@gmail.com', 'M', '01-05-1996', 4700, 5),
+	('Henrique', 'henrique@gmail.com', 'M', '07-07-1995', 3500, 6),
+	('Maria', 'maria@gmail.com', 'F', '07-07-1995', 4100, 3),
+	('Penha', 'penha@gmail.com', 'F', '07-07-1988', 5000, 5),
+	('Laura', 'laura@gmail.com', 'F', '07-07-1990', 5500, 5);
+
+SELECT funcionario.nome, funcionario.salario, departamento.nome AS departament FROM funcionario INNER JOIN departamento ON funcionario.id_departamento = departamento.id_departamento WHERE funcionario.salario >= 3000;
+
+SELECT nome, data_nascimento FROM funcionario ORDER BY data_nascimento DESC;
+
+SELECT AVG(salario) AS media_salarial FROM funcionario;
+
+-- Salário por departamento
+SELECT departamento.nome AS departamento,
+	COUNT(funcionario.id_funcionario) AS funcionarios,
+	MIN(funcionario.salario) AS  minimo,
+	MAX(funcionario.salario) AS maximo
+FROM funcionario 
+	INNER JOIN departamento ON funcionario.id_departamento = departamento.id_departamento  
+GROUP BY departamento.nome;
+
+-- Salário por sexo
+SELECT sexo, 
+	AVG(salario) AS media, 
+	COUNT(id_funcionario) AS funcionarios 
+FROM funcionario
+GROUP BY sexo;
+
+-- Salário
+SELECT COUNT(funcionario.id_funcionario) AS funcionarios,
+	funcionario.id_departamento AS departamento
+FROM funcionario
+WHERE funcionario.sexo = 'M' AND funcionario.salario > 3800
+GROUP BY funcionario.id_departamento
+HAVING COUNT(funcionario.id_funcionario) > 1;
+
+--
+SELECT COUNT(funcionario.id_funcionario) AS funcionarias,
+	funcionario.id_departamento AS departamento,
+	AVG(funcionario.salario)
+FROM funcionario
+WHERE funcionario.email LIKE '%@gmail.com' 
+	AND funcionario.sexo = 'F'
+GROUP BY funcionario.id_departamento
+HAVING AVG(funcionario.salario) > 4000;
---
+++ Modulo 3/SQL/produtos.sql
@@ -0,0 +1,110 @@
+CREATE DATABASE produtos;
+USE produtos;
+
+CREATE TABLE categoria(
+	id_categoria INT NOT NULL IDENTITY(1, 1),
+	nome VARCHAR(255),
+	ativa CHAR(3)
+	PRIMARY KEY(id_categoria)
+);
+
+INSERT INTO categoria(nome, ativa) VALUES
+	('Informatica','[v]'),
+	('Telefonia','[v]'),
+	('Games','[v]'),
+	('TV, Audio e Video','[v]'),
+	('Impressão e imagem','[v]');
+
+CREATE TABLE produto(
+	id_produto INT NOT NULL IDENTITY(1, 1),
+	id_categoria INT,
+	descricao VARCHAR(255),
+	preco INT,
+	url_imagem VARCHAR(255)
+	PRIMARY KEY(id_produto)
+);
+
+INSERT INTO produto(id_categoria, descricao, preco) VALUES
+	(1,'SSD 128GB', 300),
+	(1,'SSD 256GB', 500),
+	(1,'KIT TECLADO E MOUSE', 600),
+	(3,'PLAYSTATION 3', 1500),
+	(2,'SAMSUNG A51', 2500),
+	(4,'TELEVISÃO LG 55"', 3000),
+	(3,'PLAYSTATION 3 1TB', 1500),
+	(2,'SAMSUNG S9', 2500),
+	(2,'IPHONE 8', 2500),
+	(2,'IPHONE 9', 3000),
+	(2,'IPHONE X', 4000),
+	(4,'TELEVISÃO SAMSUNG 32"', 1000),
+	(4,'TELEVISÃO SAMSUNG 48"', 3000),
+	(3,'PLAYSTATION 4 1TB', 2800),
+	(3,'PLAYSTATION 5 1TB', 5000),
+	(1,'Teclado Razer Gamer', 250),
+	(1,'Fone de ouvido Hyperx',170),
+	(1,'Mouse Gamer Razer', 90),
+	(1,'Mouse Gamer Razer 2400', 200);
+
+INSERT INTO produto(descricao, preco) VALUES
+	('Mouse Gamer', 280),
+	('Teclado Gamer', 390);
+
+-- Produtos com categoria (intersecção -> inner join)
+SELECT produto.descricao AS descricao,
+	categoria.nome AS categoria
+FROM produto
+	INNER JOIN categoria ON produto.id_categoria = categoria.id_categoria;
+
+-- Todos os produtos com categoria, se houver (left join)
+SELECT produto.descricao AS descricao,
+	categoria.nome AS categoria
+FROM produto
+	LEFT JOIN categoria ON produto.id_categoria = categoria.id_categoria;
+
+-- Todas as categorias, mesmo sem produto (right join)
+SELECT produto.descricao AS descricao,
+	categoria.nome AS categoria
+FROM produto
+	RIGHT JOIN categoria ON produto.id_categoria = categoria.id_categoria;
+
+-- 
+SELECT MAX(produto.preco) AS preco,
+	categoria.nome AS nome
+FROM produto
+	INNER JOIN categoria ON produto.id_categoria = categoria.id_categoria
+GROUP BY categoria.nome;
+
+-- Menor e maior preço do produto da categoria game e telefonia
+SELECT MAX(produto.preco) AS preco_maximo, 
+	MIN(produto.preco) AS preco_minimo, 
+	categoria.nome AS categoria
+FROM produto 
+	INNER JOIN categoria ON produto.id_categoria = categoria.id_categoria
+WHERE categoria.id_categoria = 3 OR categoria.id_categoria = 1
+GROUP BY categoria.nome;
+
+-- Quanto custa o maior produto não catalogado
+SELECT MAX(produto.preco) AS preco
+FROM produto
+	LEFT JOIN categoria ON produto.id_categoria = categoria.id_categoria
+WHERE produto.id_categoria IS NULL;
+
+-- Média de preço por categoria
+SELECT categoria.nome AS categoria,
+	AVG(produto.preco) AS media
+FROM produto
+	LEFT JOIN categoria ON produto.id_categoria = categoria.id_categoria
+GROUP BY categoria.nome;
+
+-- 
+SELECT SUM(produto.preco) AS valor_soma, 
+	categoria.nome AS categoria
+FROM produto 
+	INNER JOIN categoria ON produto.id_categoria = categoria.id_categoria
+WHERE (categoria.id_categoria = 3
+	OR categoria.id_categoria = 4)
+	AND produto.preco > 1500
+GROUP BY categoria.nome;
+
+-- Mouse mais caro
+SELECT * FROM produto
